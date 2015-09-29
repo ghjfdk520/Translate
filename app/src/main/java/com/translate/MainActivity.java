5b0a0db6f626ee87432349381da712cb0d292523
DongZ <ghjfdk520@163.com>
2015-09-29 17:23:00 +0800

    company

--- app/src/main/java/com/translate/MainActivity.java
+++ app/src/main/java/com/translate/MainActivity.java
@@ -2,8 +2,14 @@ package com.translate;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 import com.apkfuns.logutils.LogUtils;
 import com.translate.connector.HttpCallBack;
@@ -12,7 +18,9 @@ import com.translate.database.SQLdm;
 
 
 public class MainActivity extends AppCompatActivity {
+    private ListView listView;
 
+    private CardView input;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +38,35 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
+
+
+        listView = (ListView) findViewById(R.id.list);
+        input = (CardView) findViewById(R.id.input);
+        BaseAdapter baseAdapter = new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return 10;
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return 0;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.cardview,null);
+                return view;
+            }
+        };
+        listView.setAdapter(baseAdapter);
 
     }
 
